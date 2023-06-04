a296f3b0208fa27dab6fd22bb23ea04eff28bb38
Anshuk Sharma <annshuks@gmail.com>
2023-06-04 20:15:20 +0530

    added context and routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import { useQuery } from "react-query";
-import { FormProvider, useForm } from "react-hook-form";
-
-import { fetchRecords } from "services/fetchRecords";
-import { SearchTable } from "SearchTable";
-import { SearchForm } from "SearchForm";
-import { usePatientContext } from "PatientProvider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { Dashboard } from "Pages/Dashboard";
-
-
-
+import { PatientDetails } from "PatientDetails";
 
 const App = () => (
   <div className="App">
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Dashboard /> } />
+        <Route path="/:id" element={ <PatientDetails /> } />
       </Routes>
     </BrowserRouter>
   </div >
--- src/SearchTable/index.js
+++ src/SearchTable/index.js
@@ -1,11 +1,23 @@
 import { memo } from "react";
 import { Table } from "reactstrap"
 import { useWatch } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 import { usePatientContext } from "PatientProvider";
 
 
 export const SearchTable = memo(() => {
-    const { dispatch } = usePatientContext()
+    const { dispatch } = usePatientContext();
+    const navigate = useNavigate();
+
+    const handleClick = (item = {}) => {
+        dispatch({
+            type: 'formPayload',
+            payload: { usergender, userage, userQuery, sorting, selectedRow: item }
+        });
+
+        navigate(`/${item.patient_id}`)
+    }
 
     const [usergender = '', userage = '', records = [], userQuery = '', sorting] = useWatch({
         name: ['usergender', 'userage', 'records', 'userQuery', 'sorting']
@@ -20,10 +32,7 @@ export const SearchTable = memo(() => {
                     <tr className="table-dark"><th>ID</th><th>Name</th></tr>
                 </thead>
                 <tbody>
-                    { records.map((item) => <tr key={ item.patient_id } onClick={ () => dispatch({
-                        type: 'formPayload',
-                        payload: { usergender, userage, userQuery, sorting, selectedRow: item }
-                    }) }>
+                    { records.map((item) => <tr key={ item.patient_id } onClick={ () => handleClick(item) }>
                         <td>{ item.patient_id }</td>
                         <td>{ `${item.first_name} ${item.last_name}` }</td>
                     </tr>) }
